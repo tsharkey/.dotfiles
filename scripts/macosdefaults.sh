f64430045820940fabddd22c36144bc9b727b196
Thomas Sharkey <thomassharkey@Thomass-MacBook-Pro.local>
2022-06-02 09:04:28 -0400

    fixed plists

--- scripts/macosdefaults.sh
+++ scripts/macosdefaults.sh
@@ -2,7 +2,12 @@
 
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
+
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
+
+# Make scroll direction natural
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
 # TODO: Scroll Direction
 
