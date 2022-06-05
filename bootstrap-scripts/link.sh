39007ee1da62dbb815d53601f7dbf5c56a309b14
Tom Sharkey <sharkey.thomasm@gmail.com>
2022-06-04 22:20:44 -0400

    fixup

--- bootstrap-scripts/link.sh
+++ bootstrap-scripts/link.sh
@@ -2,19 +2,25 @@
 
 # check if symlink already exists, if not create it
 ensuresymlink() {
-    if [ -L "$1" ]; then
+    if [ -L "$2" ]; then
         echo "Symlink already exists: $1"
     else
-        ln -sf "$2" "$1"
+        ln -sf "$1" "$2"
     fi
 }
 
+# check if DEV_DIRECTOY is set
+if [ -z "$DEV_DIRECTORY" ]; then
+    echo "DEV_DIRECTORY is not set"
+    exit 1
+fi
+
 # link zsh files
-ensuresymlink ./zsh/.zshrc ~/.zshrc
-ensuresymlink ./zsh/.zprofile ~/.zprofile
+ensuresymlink "$DEV_DIRECTORY"/.dotfiles/zsh/.zshrc ~/.zshrc
+ensuresymlink "$DEV_DIRECTORY"/.dotfiles/zsh/.zprofile ~/.zprofile
 
 # link git files
-ensuresymlink ./git/.gitconfig ~/.gitconfig
-ensuresymlink ./git/.gitignore ~/.gitignore
+ensuresymlink "$DEV_DIRECTORY"/.dotfiles/git/.gitconfig ~/.gitconfig
+ensuresymlink "$DEV_DIRECTORY"/.dotfiles/git/.gitignore ~/.gitignore
 
 printf "\e[32m✓ finished setting up symlinks\e[0m\n"
